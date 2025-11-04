7d863e677c49bc16f79324ad72aa49bc0ce24f39
Caio <caiodias9713@gmail.com>
2025-11-04 09:57:30 -0300

    Update dependencies in api-gateway: add @nestjs/axios and axios, and modify CORS origin comment in main.ts for clarity.

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // front ou swagger
+    origin: 'http://localhost:3001', // ou a URL do front
     credentials: true,
   });
 
