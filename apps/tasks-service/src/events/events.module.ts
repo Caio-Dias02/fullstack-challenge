bb243228681ec198346a480ed60339b5451d0e99
Caio <caiodias9713@gmail.com>
2025-11-06 08:45:03 -0300

    Update package.json and pnpm-lock.yaml to add TypeScript type definitions for cookie-parser, express, jest, node, and supertest; upgrade axios and eslint to latest versions; modify socket.io version in notifications service; enhance events module with TypeOrm integration for task entity.

--- apps/tasks-service/src/events/events.module.ts
+++ apps/tasks-service/src/events/events.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsService } from './events.service';
+import { Task } from '../tasks/entities/task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])],
   providers: [EventsService],
   exports: [EventsService],
 })
